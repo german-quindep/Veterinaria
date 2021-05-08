36e4e7cb9c9a5122894d5a81a0ecc91247abdcfb
german.quinde <german.quindep@ug.edu.ec>
2021-05-07 21:46:53 -0500

    Proyecto veterinaria V1.0

--- Frontend/src/app/components/cliente/users-mascotas-list/users-mascotas-list.component.ts
+++ Frontend/src/app/components/cliente/users-mascotas-list/users-mascotas-list.component.ts
@@ -24,7 +24,6 @@ export class UsersMascotasListComponent implements OnInit {
   ngOnInit(): void {
     this.getAllMascotas();
   }
-
   getAllMascotas() {
     const user = this.authServi.getCurrentUser();
     this.username = user.result[0].username;
